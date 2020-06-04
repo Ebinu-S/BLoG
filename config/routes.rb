62288699c94500d9930c019805e6a250ac13d950
Ebinu-s <ebinusuneer2nd@gmail.com>
2020-06-04 21:28:27 +0530

    adding photos

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
   get '/users/sign_out' => 'devise/sessions#destroy'
+  get '/users/sign_up' => 'evise/sessions#destroy'
   end
   resources :posts
 
